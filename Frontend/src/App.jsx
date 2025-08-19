aeab26895e78bafafa2534c39943d53ae56292c1
sonukathat <skkhankathat93@gmail.com>
2025-08-19 12:18:45 +0530

    resolve conflict

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,11 +1,14 @@
 import Login from "./components/Login"
 import Signup from "./components/Signup"
+import Home from "./components/Home"
+
 
 function App() {
   return (
     <>
      {/* <Signup/> */}
-     <Login/>
+     {/* <Login/> */}
+     <Home/>
     </>
   )
 }
